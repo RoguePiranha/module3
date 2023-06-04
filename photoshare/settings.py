eb14c9d197649f555d6d709702e66caaa1f38305
Andrew S <96598928+RoguePiranha@users.noreply.github.com>
2023-06-04 00:21:56 -0600

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -8,9 +8,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '09cdb70c63848b7fff7f1849075ba268f72c9bc29b547d34'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
